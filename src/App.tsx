c894528c10e5005659f651a5a715851e40861a98
Zea <zeaear714@gmail.com>
2022-04-06 17:36:28 +0900

    Save

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React, {FC} from "react"
+import { StyleSheet, Text, View } from "react-native"
+import {WebView} from "react-native-webview"
+
+const App:FC = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.statusbar}>
+        <Text style={styles.titleText}>카이마루 오늘의 식단</Text>
+      </View>
+      <WebView
+        source={{uri:"https://www.kaist.ac.kr/kr/html/campus/053001.html?dvs_cd=fclt"}}
+        style={styles.menuTable}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1, 
+    backgroundColor:"#fff"
+  },
+  statusbar:{
+    margin:30,
+    height:'10%'
+  },
+  titleText:{
+    fontSize:24,
+    fontWeight:'bold'
+  },
+  menuTable:{
+    
+  }
+
+})
+
+export default App
